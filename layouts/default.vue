95c77be6d11ca427a943d1a6d4033a9764208f50
kiri10ten <126086612+kiri10ten@users.noreply.github.com>
2023-08-06 16:51:57 +0530

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+<!-- navigation -->
+<nav class="bg-transparent mt-5 p-4">
+    <ul class="flex space-x-4 is-justify-space-between  ml-24 gap-4">
+        <li class="text-black text-xs font-bold pr-4" >
+            <NuxtLink to="/"> ホメウ</NuxtLink>
+        </li>
+        <li class="text-black text-xs font-bold pr-4">
+                <NuxtLink to="/Aboutus"> 私たちに関しては</NuxtLink>
+            </li>
+            <li class="text-black text-xs font-bold pr-4">
+                    <NuxtLink to="/"> 科目</NuxtLink>
+                </li>
+                    <li class="text-black text-xs font-bold pr-4">
+                        <NuxtLink to="/Home">アーカイブ</NuxtLink>
+                    </li>
+                    <li class="text-black text-xs font-bold pr-4">
+                            <NuxtLink to="/Home"> イベント</NuxtLink>
+                        </li>
+        
+        
+        
+    </ul>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--  
+insert everthing here        -->
+<div  class=" flex ml-28">
+    
+    <slot />
+</div>
+
+
+
+
+
+</template>
+
+<style>
+body{
+    background-color: #F5F1E9;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+ 
+  <div class="container w-full h-screen flex flex-col md:flex-row">
+
+   <div class="split h-1/2 md:w-1/2 md:h-full">
+      <div class="">
+        <img src="logo.svg ">
+        <form class="m-12" action="">
+            <input type="email" placeholder="I loved " class=" px-4 py-2 w-3/4 rounded-full border border-slate-800">
+            <button class="px-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+    <path d="M8.25 10.875a2.625 2.625 0 115.25 0 2.625 2.625 0 01-5.25 0z" />
+    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.125 4.5a4.125 4.125 0 102.338 7.524l2.007 2.006a.75.75 0 101.06-1.06l-2.006-2.007a4.125 4.125 0 00-3.399-6.463z" clip-rule="evenodd" />
+  </svg>
+  </button>
+        </form >
+        <div class="m-12 flex flex-col-4 space-x-6">
+        <button class="bg-blue">chemi</button>
+        <button>beauty</button>
+        <button>sub </button>
+        <button>ahhaha</button>
+        </div>
+        </div>
+
+   </div> 
+    
+  
+    <!-- Add any content for the first section here -->
+
+   <div class="split h-1/2 md:w-1/2 md:h-full bg-red-200">
+
+   </div>
+
+  </div>
+    
+    
+</template>
+
+<script>
+
+</script>
+
+<style>
+.almond {
+  background-color: #F5F1E9;
+}
+
+.apple {
+  background-color: #F5F1E9;
+}
+
+/* Add any custom styles for the entire component or individual sections */
+</style>
